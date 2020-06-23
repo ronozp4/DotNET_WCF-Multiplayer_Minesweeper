da72ed0a74ee8d5d4d15791c779db4fa346d28da
ronozp4 <44544338+ronozp4@users.noreply.github.com>
2020-06-23 18:18:23 +0300

    Add files via upload

---
+++ MineHost/MineHost/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WcfMineServer;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace MineHost
+{
+  //Main Host
+    public partial class MainWindow : Window
+    {
+        ServiceHost host = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (host != null)
+            {
+                host.Close();
+            }
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try { 
+            host = new ServiceHost(typeof(WcfMineServer.MineService));
+            host.Description.Behaviors.Add(
+                new ServiceMetadataBehavior { HttpGetEnabled = true });
+            host.Open();
+            label1.Content = "Host connected";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Could not access server");
+                this.Close();
+            }
+        }
+    }
+}
