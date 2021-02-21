752be25df2a41f7de5eaa80d22925254c6b0d2e7
Ron <ronozp4@gmail.com>
2021-02-21 19:53:55 +0200

    Changes

---
+++ WcfMineServer/WcfMineServer/UserFaultException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfMineServer
+{
+    //User Fault Exception
+
+    [DataContract]
+    public class UserFaultException
+    {
+        [DataMember]
+        public string Message { get; set; }
+    }
+}
